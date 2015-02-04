5be672ae68813f2b85a7f4ff42c86b7e949c1de7
Luis Hernández <luishernandez.mail@gmail.com>
2015-02-04 18:23:41 +0100

    Added test for empty widget init

--- tests/DygraphsWidgetTest.php
+++ tests/DygraphsWidgetTest.php
@@ -7,8 +7,10 @@ class DygraphsWidgetTest extends PHPUnit_Framework_TestCase {
 		$this->controller = new Controller('test');
 	}
 	
-	public function testInit() {
+	public function testEmptyInit() {
 		$widget = $this->controller->beginWidget('DygraphsWidget');
 		$this->assertInstanceOf('DygraphsWidget', $widget);
+		$this->assertEquals('yw0', $widget->htmlOptions['id']);
+		$this->assertEquals('dygraphs_yw0', $widget->jsVarName);
 	}
 }
