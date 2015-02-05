c3c8ecae65ad06f6cfe97a74160a8d98b98a4fc0
Luis Hernández <luishernandez.mail@gmail.com>
2015-02-05 20:20:39 +0100

    Cleaned up test environment

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// change the following paths if necessary
-$yiit='C:/wamp/www/framework/yiit.php';
-$config=dirname(__FILE__).'/test.php';
-
-require_once($yiit);
-
-Yii::createWebApplication($config);
